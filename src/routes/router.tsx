cd35d677469db411af8741c9d0908bb98cb0533f
Mostafa Milly <mostafamilly6@gmail.com>
2023-04-28 10:00:47 +0300

    WIP folder structure

---
+++ src/routes/router.tsx
@@ -0,0 +1,21 @@
+import { MainLayout } from "@/layouts/MainLayout";
+import { Dashboard } from "@/pages/dashboard/Dashboard";
+import { Login } from "@/pages/login/Login";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/tests/setup.ts',
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
 })
