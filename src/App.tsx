d0daecb71610f53a8297f758d78d98697000f441
Mostafa Milly <mostafamilly6@gmail.com>
2023-04-28 14:18:07 +0300

    WIP project theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <RouterProvider router={router} />
-    </>
+    </ApolloProvider>
   );
 }
 
---
+++ src/theme/typography.ts
@@ -0,0 +1,53 @@
+import { TypographyOptions } from "@mui/material/styles/createTypography";
+
+export const PRIMARY_FONT = "Lato , sans-serif";
+export const SECONDRY_FONT = "Public Sans , sans-serif";
+export const THIRD_FONT = "Inter , sans-serif";
+
+const typography: TypographyOptions = {
+  fontFamily: PRIMARY_FONT,
+  fontWeightRegular: 400,
+  fontWeightMedium: 600,
+  fontWeightBold: 700,
+  allVariants: {
+    letterSpacing: 0,
+  },
+  h1: {
+    fontFamily: PRIMARY_FONT,
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+
+  h2: {
+    fontFamily: PRIMARY_FONT,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  h3: {
+    fontFamily: PRIMARY_FONT,
+    fontWeight: 600,
+    fontSize: 16,
+  },
+  h4: {
+    fontFamily: PRIMARY_FONT,
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+  body1: {
+    fontFamily: PRIMARY_FONT,
+    fontWeight: 600,
+    fontSize: 16,
+  },
+  body2: {
+    fontFamily: PRIMARY_FONT,
+    fontWeight: "regular",
+    fontSize: 14,
+  },
+  button: {
+    fontFamily: PRIMARY_FONT,
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+};
+
+export default typography;
