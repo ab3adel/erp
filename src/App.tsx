d6c75b01a4389e9950a2c82e0ccda04ee5474bd3
Mostafa Milly <m.milly@turnsole.tech>
2023-05-04 14:11:03 +0300

    Send token in request headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import { RouterProvider } from "react-router-dom";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { ThemeProvider, responsiveFontSizes, CssBaseline } from "@mui/material";
 import { router } from "./routes";
 import { theme } from "./theme";
 import { SnackbarProvider } from "notistack";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: import.meta.env.VITE_BACKEND_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
