7298088609d0ab0cfc5ba018a719dc06f7329fe4
Mostafa Milly <m.milly@turnsole.tech>
2023-05-04 14:38:12 +0300

    Fix tabs

--- src/shared/components/curvedTabs/components/List.tsx
+++ src/shared/components/curvedTabs/components/List.tsx
@@ -7,7 +7,13 @@ export const CurvedTabList = styled(Tabs)(({ theme }) => ({
   "& .MuiTabs-root": {
     borderBottom: `1px solid ${theme.palette.divider}`,
   },
-  "& .MuiTabs-scroller button:not(:first-child)": {
+  "& .MuiTabs-scroller button": {
+    margin: 0,
+  },
+  "& .MuiTabs-scroller button:nth-child(2)": {
+    margin: "0 40px",
+  },
+  [`& .MuiTabs-scroller button:not(:first-child):not(:last-child)`]: {
     margin: "0 40px",
   },
 }));
