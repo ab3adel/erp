67d49732bf1849e47900c5d980caa4884d0deb35
Mostafa Milly <m.milly@turnsole.tech>
2023-05-23 10:01:21 +0300

    Fix create view bug

--- src/pages/relationships/views/custom-view/AccountsCustomView.tsx
+++ src/pages/relationships/views/custom-view/AccountsCustomView.tsx
@@ -3,10 +3,14 @@ import AddIcon from "@mui/icons-material/Add";
 import { AccountsCustomViewTable } from "./components/AccountsCustomViewTable";
 import { useAddAccount } from "./hooks/useAddAccount";
 import { useGridApiRef } from "@mui/x-data-grid";
+import { useSearchParams } from "react-router-dom";
 
 export const AccountsCustomView = () => {
   const apiRef = useGridApiRef();
   const { dispatch, isRowAdded } = useAddAccount(apiRef);
+  const [params , setParams] = useSearchParams();
+  const tabParam = params.get("tab");
+
   return (
     <Box>
       <Button
@@ -19,6 +23,7 @@ export const AccountsCustomView = () => {
         NEW ACCOUNT
       </Button>
       <AccountsCustomViewTable
+        key={tabParam}
         dispatch={dispatch}
         isRowAdded={isRowAdded}
         apiRef={apiRef}
