a548d707ea80e8e0b791b225a8c34b9ce6aa15bc
ammaramame <ammaramame@gmail.com>
2023-07-07 21:08:37 +0300

    add approve & merge

---
+++ src/pages/managment/views/receptions/containers/approveLots/ApproveLots.tsx
@@ -0,0 +1,33 @@
+import ApproveReceiptions from "../../components/ApproveReceiptions/ApproveReceiptions";
+import { useLogic } from "./ApprovedLots.logic";
+
+const ApproveLots = () => {
+  const { clearIdList, idList, form, loading } = useLogic();
+  return (
+    <ApproveReceiptions
+      open={idList.length > 0}
+      type={idList.length === 1 ? "single" : "selection"}
+      onCancelClick={clearIdList}
+      approveAndCombineProps={{
+        value: form.values["combined"],
+        name: "combined",
+        onChange: form.handleChange,
+      }}
+      withCombine={form.values.combined}
+      totalWeight={}
+      confirmButtonProps={{
+        onClick: form.submitForm,
+        disabled: !form.isValid || loading,
+      }}
+      textfieldProps={{
+        value: form.values["name"],
+        name: "name",
+        onChange: form.handleChange,
+        error: !!form.errors.name,
+        helperText: form.errors.name,
+      }}
+    />
+  );
+};
+
+export default ApproveLots;
