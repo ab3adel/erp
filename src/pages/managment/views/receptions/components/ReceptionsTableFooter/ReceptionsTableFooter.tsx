d8d53cdf8ee8157064ecbe5253870e08fbe44b78
iammouaz <m.molkey@turnsole.tech>
2023-07-09 21:23:05 +0300

    change table footer comnponent

--- src/pages/managment/views/receptions/components/ReceptionsTableFooter/ReceptionsTableFooter.tsx
+++ src/pages/managment/views/receptions/components/ReceptionsTableFooter/ReceptionsTableFooter.tsx
@@ -2,15 +2,15 @@ import { Box } from "@mui/material";
 import { GridFooter } from "@mui/x-data-grid-pro";
 import { GridApiPro } from "@mui/x-data-grid-pro/models/gridApiPro";
 import { FunctionComponent } from "react";
+import FormulaFooter from "../formulaFooter/FormulaFooter";
 
 interface ReceptionsTableFooterProps {
   apiRef: React.MutableRefObject<GridApiPro>;
 }
 
-const ReceptionsTableFooter: FunctionComponent<
-  ReceptionsTableFooterProps
-> = ({apiRef}) => {
-
+const ReceptionsTableFooter: FunctionComponent<ReceptionsTableFooterProps> = ({
+  apiRef,
+}) => {
   const containerStyle = {
     px: "24px",
     py: "14px",
@@ -20,29 +20,19 @@ const ReceptionsTableFooter: FunctionComponent<
     borderTop: "1px solid #dcdcdc",
     borderBottom: "1px solid #dcdcdc",
   };
-  const valueChip = (value: string) => (
-    <Box
-      bgcolor="#EEEEEE"
-      color="#757777"
-      px="8px"
-      fontSize="16px"
-      fontWeight="700"
-      borderRadius="4px"
-    >
-      {value}
-    </Box>
-  );
 
-  console.log(apiRef.current.getSelectedRows())
-
+  console.log(apiRef.current.getSelectedRows()); // We will need this after connecting to the backend
   return (
     <Box mt="auto" display="flex" flexDirection="column">
       <Box sx={{ ...containerStyle }}>
-        {valueChip("Total Quantity: 14,123.45 Kg")}
-        {valueChip("/")}
-        {valueChip("Total Weight: 34,568.45 Kg")}
-        {valueChip("=")}
-        {valueChip("Total Average Unit Cost: 2.45 USD/Kg")}
+        <FormulaFooter
+          formulaList={[
+            "Total Cost: USD$84,568.45",
+            "/",
+            "Total Weight: 34,568.45 Kg",
+          ]}
+          result="Total Average Unit Cost: 2.45 USD/Kg"
+        />
       </Box>
       <GridFooter />
     </Box>
